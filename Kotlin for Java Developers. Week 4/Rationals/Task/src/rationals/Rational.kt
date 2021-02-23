0c6aa707645bbcd79e9031ebf7ab125d9d241c55
Ahmed Elsafty <ahmed0elsafty@users.noreply.github.com>
2021-02-23 22:52:44 +0200

    Complete Rational Task

---
+++ Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/Rational.kt
@@ -0,0 +1,140 @@
+package rationals
+
+import java.math.BigInteger
+
+
+class Rational(n: BigInteger, d: BigInteger = BigInteger.ONE) : Comparable<Rational> {
+    val numerator: BigInteger
+    val denominator: BigInteger
+
+    init {
+        val gcd = n.gcd(d)
+        when {
+            n > BigInteger.ZERO && d < BigInteger.ZERO -> {
+                numerator = (n * -BigInteger.ONE) / gcd
+                denominator = (d * -BigInteger.ONE) / gcd
+            }
+            n < BigInteger.ZERO && d < BigInteger.ZERO -> {
+                numerator = (n * -BigInteger.ONE) / gcd
+                denominator = (d * -BigInteger.ONE) / gcd
+            }
+            else -> {
+                numerator = n / gcd
+                denominator = d / gcd
+            }
+        }
+    }
+
+    override fun toString(): String {
+        return when (denominator) {
+            BigInteger.ONE -> "$numerator"
+            else -> "$numerator/$denominator"
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is Rational) {
+            numerator == other.numerator && denominator == other.denominator
+        } else false
+    }
+
+    override fun compareTo(other: Rational): Int {
+        return when {
+            numerator * other.denominator > other.numerator * denominator -> 1
+            numerator * other.denominator < other.numerator * denominator -> -1
+            else -> 0
+        }
+    }
+    operator fun plus(other: Rational): Rational {
+        val num = numerator * other.denominator + other.numerator * denominator
+        val den = denominator * other.denominator
+        val gcd = num.gcd(den)
+        return Rational(num / gcd, den / gcd)
+    }
+
+    operator fun minus(other: Rational): Rational {
+        val n = numerator * other.denominator - other.numerator * denominator
+        val d = denominator * other.denominator
+        val gcd = n.gcd(d)
+        return Rational(n / gcd, d / gcd)
+    }
+
+    operator fun times(other: Rational): Rational {
+        val n = numerator * other.numerator
+        val d = denominator * other.denominator
+        val gcd = n.gcd(d)
+        return Rational(n / gcd, d / gcd)
+    }
+
+    operator fun div(other: Rational): Rational {
+        val n = numerator * other.denominator
+        val d = other.numerator * denominator
+        val gcd = n.gcd(d)
+        return Rational(n / gcd, d / gcd)
+    }
+
+    operator fun unaryMinus(): Rational {
+        return Rational(numerator * -BigInteger.ONE, denominator)
+    }
+
+
+    override fun hashCode(): Int {
+        var result = numerator.hashCode()
+        result = 31 * result + denominator.hashCode()
+        return result
+    }
+
+}
+
+fun String.toRational(): Rational {
+    val rational = split("/")
+    return when {
+        rational.size == 1 -> Rational(rational[0].toBigInteger())
+        else -> Rational(rational[0].toBigInteger(), rational[1].toBigInteger())
+    }
+}
+
+infix fun Int.divBy(d: Int): Rational = Rational(toBigInteger(), d.toBigInteger())
+
+infix fun Long.divBy(d: Long): Rational = Rational(toBigInteger(), d.toBigInteger())
+
+infix fun BigInteger.divBy(d: BigInteger): Rational = Rational(this, d)
+
+fun main() {
+    val half = 1 divBy 2
+    val third = 1 divBy 3
+
+    val sum: Rational = half + third
+    println(5 divBy 6 == sum)
+
+    val difference: Rational = half - third
+    println(1 divBy 6 == difference)
+
+    val product: Rational = half * third
+    println(1 divBy 6 == product)
+
+    val quotient: Rational = half / third
+    println(3 divBy 2 == quotient)
+
+    val negation: Rational = -half
+    println(-1 divBy 2 == negation)
+
+    println((2 divBy 1).toString() == "2")
+    println((-2 divBy 4).toString() == "-1/2")
+    println("117/1098".toRational().toString() == "13/122")
+
+    val twoThirds = 2 divBy 3
+    println(half < twoThirds)
+
+    println(half in third..twoThirds)
+
+    println(2000000000L divBy 4000000000L == 1 divBy 2)
+
+    println(
+        "912016490186296920119201192141970416029".toBigInteger() divBy
+                "1824032980372593840238402384283940832058".toBigInteger() == 1 divBy 2
+    )
+}
+
+
+
